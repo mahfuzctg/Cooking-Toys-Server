42ae248b435e92bfd006f016175383568843e420
Mahfuz <aamahfuz.pro@gmail.com>
2023-05-21 01:56:35 +0600

    connected addtoy with clint

--- index.js
+++ index.js
@@ -25,6 +25,14 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+
+    const toyCollection = client.db("toyDB").collection("toy");
+    app.post("/addtoys", async (req, res) => {
+      const newToys = req.body;
+      console.log(newToys);
+      const result = await toyCollection.insertOne(newToys);
+      res.send(result);
+    });
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log(
@@ -32,7 +40,8 @@ async function run() {
     );
   } finally {
     // Ensures that the client will close when you finish/error
-    await client.close();
+    // await
+    // client.close();
   }
 }
 run().catch(console.dir);
