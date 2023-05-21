e90a540a5d78fa48e95966d034d7af2469489d9d
Mahfuz <aamahfuz.pro@gmail.com>
2023-05-21 12:37:46 +0600

    connected view detail with client

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const cors = require("cors");
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 require("dotenv").config();
 const app = express();
 const port = process.env.PORT || 5000;
@@ -33,6 +33,15 @@ async function run() {
       const result = await cursor.toArray();
       res.send(result);
     });
+    //
+    app.get("/addtoys/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await toyCollection.findOne(query);
+      res.send(result);
+    });
+    //
+
     app.post("/addtoys", async (req, res) => {
       const newToys = req.body;
       console.log(newToys);
